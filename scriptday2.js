7032be440207acfdb23d29076135ff9b3fc6474f
Ajay Sanjay Shelke <ajayshelke8275@gmail.com>
2025-11-12 19:08:57 +0530

    Add files via upload

---
+++ scriptday2.js
@@ -0,0 +1,63 @@
+console.log("hello world");
+
+//  voting machine
+
+let age =180;
+
+if (age>=18 && age <120){
+    console.log("Eligibale for voting....!");
+}else{
+    console.log("Not eligiblefor voting");
+} 
+
+4
+100
+400
+
+
+
+let yr=2001;
+
+if(yr %4==0||ifyr %4==0||ifyr %4==0||){
+    console.log("This is leap year");
+}else{
+     console.log("This is not leap year");
+}
+
+
+let yr =2020;
+if(yr %4==0||ifyr %4==0||ifyr %4==0||){
+    console.log("This is leap year");
+}else{
+     console.log("This is not leap year");
+}
+
+
+
+// day
+
+let day="monday";
+switch(day){
+    case:"monday"
+    console.log("monday....!");
+    break;
+     case:"Tuesaday"
+    console.log("Tuesday....!");
+    break;
+     case:"wenseday"
+    console.log("wenseday....!");
+    break;
+     case:"Thursaday"
+    console.log("Thursaday....!");
+    break;
+     case:"Friday"
+    console.log("Friday....!");
+    break;
+     case:"saturday"
+    console.log("saturday....!");
+    break;
+     case:""
+    console.log("monday....!");
+    break;
+}
+
