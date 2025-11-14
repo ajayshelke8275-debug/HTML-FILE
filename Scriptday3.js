499f5a2d7f7cbf04f0b408b8b5be655a9696bc23
Ajay Sanjay Shelke <ajayshelke8275@gmail.com>
2025-11-14 10:31:30 +0530

    Add files via upload

---
+++ Scriptday3.js
@@ -0,0 +1,54 @@
+console.log("Welcome...! ")
+console.log("-------------------")
+
+// function Hello(name){
+//     console.log( "Student Name is :  " + name);
+// }
+
+
+// Hello("rahul");
+// Hello("raj");
+// Hello("vraj");
+// Hello("sahil");
+// Hello("ravi");
+// Hello("raju");
+
+
+
+// 1. Non - Parameterised 
+// 2. Parameterised 
+// 3. Non - Parameterised with Return Type
+// 4. Parameterised with Return Type
+
+// function Add(){
+//     let a = 24;
+//     let b = 26;
+
+// return a+b;
+// }
+
+// console.log(Add());
+
+
+// Objects : 
+
+
+// keys : values
+
+//  xml , yml , html , json , text , docx  
+
+// JSON : JavaScript Object Notation 
+
+
+// Object
+
+// let obj = {
+//    "name":"raj",
+//    "age": 23,
+//    "city":"pune" 
+// }
+
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+
